fb76416895c385a35c86a7a2811b457d0e2d1f23
JP Barbosa <jp@jp7.com.br>
2022-01-10 15:17:21 -0300

    Add resolvers to findOne, update and remove article

--- src/resolvers/ArticleResolver.ts
+++ src/resolvers/ArticleResolver.ts
@@ -1,4 +1,4 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { Arg, ID, Mutation, Query, Resolver } from 'type-graphql';
 import { Repository } from 'typeorm';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import { Article } from '../entities/Article';
@@ -14,10 +14,48 @@ export class ArticleResolver {
     return this.articleRepository.find();
   }
 
+  @Query((returns) => Article)
+  async article(@Arg('id', () => ID) id: string): Promise<Article> {
+    const article = await this.articleRepository.findOne(id);
+
+    if (!article) {
+      throw new Error(`Article with id ${id} not found`);
+    }
+
+    return article;
+  }
+
   @Mutation((returns) => Article)
   createArticle(
     @Arg('data', () => ArticleInput) articleInput: ArticleInput
   ): Promise<Article> {
     return this.articleRepository.save(articleInput);
+  }
+
+  @Mutation((returns) => Article)
+  async updateArticle(
+    @Arg('id', () => ID) id: string,
+    @Arg('data', () => ArticleInput) articleInput: ArticleInput
+  ): Promise<Article> {
+    const article = await this.articleRepository.findOne(id);
+
+    if (!article) {
+      throw new Error(`Article with id ${id} not found`);
+    }
+
+    this.articleRepository.merge(article, articleInput);
+    return article.save();
+  }
+
+  @Mutation((returns) => Boolean)
+  async removeArticle(@Arg('id', () => ID) id: string): Promise<boolean> {
+    const article = await this.articleRepository.findOne(id);
+
+    if (!article) {
+      throw new Error(`Article with id ${id} not found`);
+    }
+
+    article.remove();
+    return true;
   }
 }
